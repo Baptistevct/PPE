3e35745b265897769fb47a6fbc5d582162d6e5d7
Baptistevct <baptistevincent41@gmail.com>
2025-03-26 10:19:04 +0100

    WS changé

--- app/Controllers/WebServiceController.php
+++ app/Controllers/WebServiceController.php
@@ -14,9 +14,52 @@ class WebServiceController extends BaseController {
         return $this->respond($categories);
     }
 
+
+    /**
+     * retourne un tableau de tailles pour un modele donné
+     */
+    private function tableauDeTailles($modele){
+        $tableauTailles=array();
+
+        $db = \Config\Database::connect();
+        $sql="SELECT taille FROM vetements WHERE modele = ?;";
+        $req=$db->query($sql, [$modele]);
+        $results = $req->getResult();
+        foreach($results as $ligne){
+            $taille=$ligne->taille;
+            //ajoute la taille parcourue à la fin du tableau
+            $tableauTailles[]=$taille;
+        }
+        return $tableauTailles;
+    }
+
+
+
     function getVetementParCategoriesId(int $catId){
-        $vetements = Vetement::where('idCategories','=', $catId)->get();
-        return $this->respond($vetements);
+        $chaine="[";
+        $db = \Config\Database::connect();
+        $req=$db->query("SELECT DISTINCT modele FROM vetements");
+        $results = $req->getResult();
+        $results_sz=count($results);
+        $com=0;
+        
+        foreach($results as $ligne){
+            $mod=$ligne->modele;
+
+            //nom des modeles
+            $chaine = $chaine.'{ "modele" : "'.$mod.'","tailles": ';
+
+            //recupere les tailles pour ce modele
+            $chaineTableau=json_encode($this->tableauDeTailles($mod));
+            $chaine=$chaine.$chaineTableau;
+            $chaine=$chaine.'}';
+            $com++;
+            if($com<$results_sz){
+                $chaine.=",";
+            }
+        }
+        $chaine.="]";
+        echo $chaine;
     }
 
 }
